d4808d1bd1fe56ac2a20ea31f560a8b607513548
Lee Mracek <lee.mracek@gmail.com>
2015-09-15 22:54:26 -0700

    Add Trajectory controller

--- src/org/usfirst/frc/team115/robot/RobotMap.java
+++ src/org/usfirst/frc/team115/robot/RobotMap.java
@@ -28,6 +28,20 @@ public class RobotMap {
     public static final int BACK_LEFT_DRIVE = 8;
     public static final int BACK_RIGHT_DRIVE = 5;
     
+    public static final double DRIVE_POSITION_KP = 0;
+    public static final double DRIVE_POSITION_KI = 0;
+    public static final double DRIVE_POSITION_KD = 0;
+    public static final double DRIVE_POSITION_KV = 0;
+    public static final double DRIVE_POSITION_KA = 0;
+
+    public static final double DRIVE_ON_TARGET_ERROR = 0;
+    public static final double DRIVE_MAX_ACCEL = 0;
+    public static final double DRIVE_MAX_VELOCITY = 0;
+    
+    public static final double DRIVE_STRAIGHT_KP = 0;
+    public static final double DRIVE_STRAIGHT_KI = 0;
+    public static final double DRIVE_STRAIGHT_KD = 0;
+    
     public static final int DRIVE_ENCODER_LEFT_DIOA = 0;
     public static final int DRIVE_ENCODER_LEFT_DIOB = 1;
     public static final int DRIVE_ENCODER_RIGHT_DIOA = 2;
---
+++ src/org/usfirst/frc/team115/robot/commands/DriveStraightWithTrajectory.java
@@ -0,0 +1,81 @@
+package org.usfirst.frc.team115.robot.commands;
+
+
+
+import org.usfirst.frc.team115.lib.SynchronousPID;
+import org.usfirst.frc.team115.lib.trajectory.TrajectoryFollower;
+import org.usfirst.frc.team115.lib.trajectory.TrajectoryFollower.TrajectorySetpoint;
+import org.usfirst.frc.team115.lib.trajectory.TrajectoryFollowingPositionController;
+import org.usfirst.frc.team115.robot.Robot;
+import org.usfirst.frc.team115.robot.RobotMap;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class DriveStraightWithTrajectory extends Command {
+	private final double goal;
+
+	private TrajectoryFollowingPositionController distanceController;
+	private SynchronousPID turnPid;
+
+	private static int ENCODER_SCALE = 1444;
+	private static double ERROR = 1;
+	private final double SCALE = (1 / (Math.PI * 8)) * 2 * ENCODER_SCALE / ERROR;
+
+	public DriveStraightWithTrajectory(double goal, double maxVelocity) {
+		requires(Robot.drive);
+		TrajectoryFollower.TrajectoryConfig config = new TrajectoryFollower.TrajectoryConfig();
+		config.dt = 20 / 1000;
+		config.maxAcceleration = RobotMap.DRIVE_MAX_ACCEL;
+		config.maxVelocity = maxVelocity;
+
+		distanceController = new TrajectoryFollowingPositionController(
+				RobotMap.DRIVE_POSITION_KP,
+				RobotMap.DRIVE_POSITION_KI,
+				RobotMap.DRIVE_POSITION_KD,
+				RobotMap.DRIVE_POSITION_KV,
+				RobotMap.DRIVE_POSITION_KA,
+				RobotMap.DRIVE_ON_TARGET_ERROR,
+				config);
+
+		this.goal = goal;
+	}
+
+	@Override
+	protected void initialize() {
+		TrajectorySetpoint initialSetpoint = new TrajectorySetpoint();
+		initialSetpoint.position = Robot.drive.getDistance();
+		initialSetpoint.velocity = Robot.drive.getVelocity();
+		distanceController.setGoal(initialSetpoint, goal * SCALE);
+
+		turnPid = new SynchronousPID();
+		turnPid.setPID(RobotMap.DRIVE_STRAIGHT_KP,
+				RobotMap.DRIVE_STRAIGHT_KI,
+				RobotMap.DRIVE_STRAIGHT_KD);
+		turnPid.setSetpoint(Math.toRadians(Robot.navx.getYaw()));
+	}
+
+	@Override
+	protected void execute() {
+		distanceController.update(Robot.drive.getDistance(), Robot.drive.getVelocity());
+		double throttle = distanceController.get();
+		double turn = turnPid.calculate(Math.toRadians(Robot.navx.getYaw()));
+
+		Robot.drive.control(throttle, turn);
+	}
+
+	@Override
+	protected boolean isFinished() {
+		return distanceController.isOnTarget();
+	}
+
+	@Override
+	protected void end() {
+		Robot.drive.stop();
+	}
+
+	@Override
+	protected void interrupted() {
+		Robot.drive.stop();
+	}
+
+}
