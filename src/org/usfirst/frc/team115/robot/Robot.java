b53e216d4e155713769b13c86f157aa51b36bc44
Lee Mracek <lee.mracek@gmail.com>
2015-09-16 22:45:00 -0700

    Update navx dependency library

--- src/org/usfirst/frc/team115/robot/Robot.java
+++ src/org/usfirst/frc/team115/robot/Robot.java
@@ -8,7 +8,7 @@ import org.usfirst.frc.team115.robot.subsystems.Flag;
 import org.usfirst.frc.team115.robot.subsystems.Loader;
 import org.usfirst.frc.team115.robot.subsystems.Shooter;
 
-import com.kauailabs.navx_mxp.AHRS;
+import com.kauailabs.navx.frc.AHRS;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.SerialPort;
@@ -34,15 +34,16 @@ public class Robot extends IterativeRobot {
 	public static CompressorSystem compressor;
 	public static AHRS navx;
 
-    Command autonomousCommand; // null because I don't want to deal with this yet
-    
-    /**
+	Command autonomousCommand; // null because I don't want to deal with this
+								// yet
+
+	/**
      * This function is run when the robot is first started up and should be
      * used for any initialization code.
      */
     public void robotInit() {
     	// initializes each subsystem on the robot
-    	navx = new AHRS(new SerialPort(57600, Port.kMXP));
+    	navx = new AHRS(SerialPort.Port.kMXP);
     	shooter = new Shooter();
     	loader = new Loader();
     	drive = new DriveTrain();
@@ -53,55 +54,57 @@ public class Robot extends IterativeRobot {
     	// initializes command for autonomous
 		autonomousCommand = new Autonomous();
     }
-	
-    // runs periodically when the robot is disabled
+
+	// runs periodically when the robot is disabled
 	public void disabledPeriodic() {
 		Scheduler.getInstance().run();
 	}
 
 	// Initialize and run the command at the start of autonomous
-    public void autonomousInit() {
-        // schedule the autonomous command (example)
-        if (autonomousCommand != null) autonomousCommand.start();
-    }
+	public void autonomousInit() {
+		// schedule the autonomous command (example)
+		if (autonomousCommand != null)
+			autonomousCommand.start();
+	}
 
-    /**
-     * This function is called periodically during autonomous
-     */
-    public void autonomousPeriodic() {
-        Scheduler.getInstance().run();
-    }
+	/**
+	 * This function is called periodically during autonomous
+	 */
+	public void autonomousPeriodic() {
+		Scheduler.getInstance().run();
+	}
 
-    /**
-     * This method is called once when teleop starts
-     */
-    public void teleopInit() {
+	/**
+	 * This method is called once when teleop starts
+	 */
+	public void teleopInit() {
 		// This makes sure that the autonomous stops running when
-        // teleop starts running. If you want the autonomous to 
-        // continue until interrupted by another command, remove
-        // this line or comment it out.
-        if (autonomousCommand != null) autonomousCommand.cancel();
-    }
+		// teleop starts running. If you want the autonomous to
+		// continue until interrupted by another command, remove
+		// this line or comment it out.
+		if (autonomousCommand != null)
+			autonomousCommand.cancel();
+	}
 
-    /**
-     * This function is called when the disabled button is hit.
-     * You can use it to reset subsystems before shutting down.
-     */
-    public void disabledInit(){
+	/**
+	 * This function is called when the disabled button is hit. You can use it
+	 * to reset subsystems before shutting down.
+	 */
+	public void disabledInit() {
 
-    }
+	}
 
-    /**
-     * This function is called periodically during operator control
-     */
-    public void teleopPeriodic() {
-        Scheduler.getInstance().run();
-    }
-    
-    /**
-     * This function is called periodically during test mode
-     */
-    public void testPeriodic() {
-        LiveWindow.run();
-    }
+	/**
+	 * This function is called periodically during operator control
+	 */
+	public void teleopPeriodic() {
+		Scheduler.getInstance().run();
+	}
+
+	/**
+	 * This function is called periodically during test mode
+	 */
+	public void testPeriodic() {
+		LiveWindow.run();
+	}
 }
